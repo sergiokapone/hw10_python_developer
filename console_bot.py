9980c06d7aad6c121c70e9f6c96cac8301a3f2ff
Sergiy <sergiy.ponomarenko@gmail.com>
2023-02-20 11:10:40 +0200

    Update console_bot.py

--- console_bot.py
+++ console_bot.py
@@ -22,9 +22,6 @@ class Field:
     def __init__(self, value: str):
         self.value = value
 
-    def __eq__(self, other):
-        return self.value == other.value
-
     def __repr__(self):
         return f"{self.value}"
 
